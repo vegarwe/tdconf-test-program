3e40ea4db9f7240482d0509702af86073ac1d975
Vegar Westerlund <vegarwe@gmail.com>
2015-07-29 23:00:10 +0200

    Fix up Javascript part. Update index.html from the generated version

--- program.js
+++ program.js
@@ -9,9 +9,9 @@
                 console.log('favourites ' + Y.JSON.stringify(favourites));
 
                 Y.all("img.fav-icon").each(function (img, idx, list) {
-                    var list_id = img.ancestor().get('id');
+                    var show_id = img.ancestor('div.program-post').get('id');
 
-                    if (list_id in favourites && favourites[list_id]) {
+                    if (show_id in favourites && favourites[show_id]) {
                         img.set('src', 'http://raiom.no/tdconf-test-program/ico/star_sel.png');
                     }
                 });
@@ -19,14 +19,14 @@
                 // Handle favourite icon clicked
                 var favourite = Y.all(".favourite");
                 favourite.on("click", function (e) {
-                    var list_id = e.currentTarget.get('id');
+                    var show_id = e.currentTarget.ancestor('div.program-post').get('id');
 
-                    var img = e.currentTarget.get('children');
-                    if (list_id in favourites && favourites[list_id]) {
-                        favourites[list_id] = false;
+                    var img = e.currentTarget.one('img');
+                    if (show_id in favourites && favourites[show_id]) {
+                        favourites[show_id] = false;
                         img.set('src', 'http://raiom.no/tdconf-test-program/ico/star_unsel.png');
                     } else {
-                        favourites[list_id] = true;
+                        favourites[show_id] = true;
                         img.set('src', 'http://raiom.no/tdconf-test-program/ico/star_sel.png');
                     }
                     Y.Cookie.set('favourites', Y.JSON.stringify(favourites), { expires: new Date("January 12, 2025") });
@@ -35,13 +35,11 @@
                 // Handle expand clicked
                 var expand = Y.all(".expand");
                 expand.on("click", function (e) {
-                    var list_id     = e.currentTarget.get('id');
-                    var show_id     = 'p' + list_id.substring(1);
-
-                    var img         = e.currentTarget.one('img');
-                    var show        = Y.one('#' + show_id);
-
+                    var program = e.currentTarget.ancestor('div.program-post');
+                    var img     = program.one('img.expand-icon');
+                    var show    = program.one('div.show');
                     var hidden = show.hasClass('hidden');
+
                     if (hidden) {
                         show.removeClass('hidden');
                         img.set('src', 'http://raiom.no/tdconf-test-program/ico/arrow-up.png');
@@ -49,12 +47,6 @@
                         show.addClass('hidden');
                         img.set('src', 'http://raiom.no/tdconf-test-program/ico/arrow-down.png');
                     }
-                });
-
-                // Handle kebab clicked
-                var expand = Y.all(".kebab-icon");
-                expand.on("click", function (e) {
-                    console.log('kebab');
                 });
             });
         </script>
